079b6f033d6dd4ee9d4617faa54e5a26d5b79e45
Andress10 <andres_20nuz@hotmail.com>
2021-11-23 22:50:44 -0300

    Andres N. resubi todos los archivos por un problema de repositorio

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.listen(3000 , ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/productos', (req,res)=>{
+    res.sendFile(__dirname + '/views/productos.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
